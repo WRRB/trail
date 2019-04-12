1597b5374a4b66236c7b7482c566d41913604893
Wim Berchmans <wimberchmans@gmail.com>
2019-04-12 14:15:48 +0200

    Clean up console print func

--- trail/show.py
+++ trail/show.py
@@ -4,12 +4,18 @@ import pytz
 
 def print_to_console(board):
     dep = board.get('departures').get('departure')
-    fmt = '+{}    {}\t{}'
+    fmt = '{}  {}\t{}'
     for d in dep:
         direction = d.get('station')
-        late = int(d.get('delay'))
-        time = datetime.utcfromtimestamp(int(d.get('time')))
-        time = pytz.utc.localize(time)
-        time = time.astimezone(pytz.timezone('Europe/Brussels'))
-        print fmt.format(late/60,time.strftime( '%H:%M'), direction.encode('utf-8'), )
+        late = int(d.get('delay'))/60
+        delay_indicator = '+{}'.format(late) if late else None
+        padding = '  '
+        time = format_time(int(d.get('time')))
+        print fmt.format(delay_indicator or padding, time, direction.encode('utf-8'))
 
+
+def format_time(timestamp):
+    time = datetime.utcfromtimestamp(timestamp)
+    time = pytz.utc.localize(time)
+    time = time.astimezone(pytz.timezone('Europe/Brussels'))
+    return time.strftime('%H:%M')
